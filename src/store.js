63db9fe3fb699af05d79a5ff5da30a4d6aebeaa3
Chris Jarvis <jrvscm@gmail.com>
2018-02-05 22:26:39 -0700

    added redux devtools/work on creating reducer

--- src/store.js
+++ src/store.js
@@ -1,3 +1,11 @@
 import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import thunk from 'redux-thunk';
+
+const middleware = applyMiddleware(thunk);
+
+const store = createStore(
+    combineReducers({
+    }),
+    compose(middleware, +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
