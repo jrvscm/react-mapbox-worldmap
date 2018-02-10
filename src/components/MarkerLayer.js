f1be23bde438d6fcc1595f8e400689a4a9a70fcc
Chris Jarvis <jrvscm@gmail.com>
2018-02-09 19:03:14 -0700

    working on marker movement

--- src/components/MarkerLayer.js
+++ src/components/MarkerLayer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Feature, Layer } from 'react-mapbox-gl';
+import { Feature, Layer} from 'react-mapbox-gl';
 import { updateLocation } from '../actions/index';
 import '../reset.css';
 import './MarkerLayer.css';
